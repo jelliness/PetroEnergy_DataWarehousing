558213b928e889bfd3a6923370a852dc94b9db70
Nicole Cabansag <111856754+iskaiflakes@users.noreply.github.com>
2025-05-15 08:47:52 +0800

    Added metadata in silver layer envi

--- scripts/silver/envi_silver_layer/ddl_silver.sql
+++ scripts/silver/envi_silver_layer/ddl_silver.sql
@@ -21,7 +21,10 @@ Script Purpose:
         site_address   TEXT,
         city_town      VARCHAR(100),
         province       VARCHAR(100),
-        zip            VARCHAR(10)
+        zip            VARCHAR(10),
+        -- Metadata
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
     -- envi_company_property
@@ -30,7 +33,10 @@ Script Purpose:
         cp_id      VARCHAR(30),       -- Example: CP-PSC-001
         company_id VARCHAR(20),       -- Referenced to company_info.
         cp_name    VARCHAR(100),
-        cp_type    VARCHAR(50)        -- Example values: Equipment, Vehicle
+        cp_type    VARCHAR(50),       -- Example values: Equipment, Vehicle
+        -- Metadata
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
     -- envi_natural_sources
@@ -38,7 +44,10 @@ Script Purpose:
     CREATE TABLE silver.envi_natural_sources (
         ns_id      VARCHAR(30),       -- Example: NS-PSC-001
         company_id VARCHAR(20),       -- Referenced to company_info.
-        ns_name    VARCHAR(100)
+        ns_name    VARCHAR(100),
+        -- Metadata
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
     -- envi_water_withdrawal
@@ -50,7 +59,10 @@ Script Purpose:
         month                 VARCHAR(20),
         ns_id                 VARCHAR(30),         -- Referenced to natural sources.
         volume                DOUBLE PRECISION,    -- Allows decimal values (e.g., 123.456)
-        unit_of_measurement   VARCHAR(20)
+        unit_of_measurement   VARCHAR(20),
+        -- Metadata
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
 -- envi_diesel_consumption
@@ -62,7 +74,10 @@ CREATE TABLE silver.envi_diesel_consumption (
     unit_of_measurement  VARCHAR(20),
     consumption          DOUBLE PRECISION,     -- Allows decimal values (e.g., 234.789)
     date                 DATE,
-	month			     VARCHAR(15)
+	month			     VARCHAR(15),
+    -- Metadata
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- envi_electric_consumption
@@ -73,7 +88,10 @@ CREATE TABLE silver.envi_electric_consumption (
     unit_of_measurement	 	VARCHAR(20),
     consumption 		 	DOUBLE PRECISION,     -- Allows decimal values (e.g., 234.789)
     quarter           	 	VARCHAR(5),
-    year               	 	INT
+    year               	 	INT,
+    -- Metadata
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- envi_power_generation
@@ -84,7 +102,10 @@ CREATE TABLE silver.envi_power_generation (
     unit_of_measurement 	VARCHAR(20),
     generation 				DOUBLE PRECISION,     -- Allows decimal values (e.g., 234.789)
     quarter 				VARCHAR(5),
-    year INT
+    year INT,
+    -- Metadata
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- envi_non_hazard_waste
@@ -97,7 +118,10 @@ CREATE TABLE silver.envi_non_hazard_waste (
     unit_of_measurement 	VARCHAR(20),
     waste 					DOUBLE PRECISION,     -- Allows decimal values (e.g., 234.789)
     month 					VARCHAR(15),
-    year 					INT
+    year 					INT,
+    -- Metadata
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- envi_hazard_waste
@@ -109,7 +133,10 @@ CREATE TABLE silver.envi_hazard_waste (
     unit_of_measurement 	VARCHAR(20),
     waste 					DOUBLE PRECISION,           -- Allows decimal values (e.g., 234.789)
     quarter 				VARCHAR(2),  		 		-- Example: 'Q1', 'Q2', 'Q3', 'Q4'
-    year 					INT
+    year 					INT,
+    -- Metadata
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- envi_activity
@@ -118,7 +145,10 @@ CREATE TABLE silver.envi_activity (
     ea_id 			        VARCHAR(30),            -- Example: EA_PSC_001
 	metrics 				VARCHAR(20),
     company_id 				VARCHAR(20),       	    -- Referenced to company_info.
-    envi_act_name 			TEXT
+    envi_act_name 			TEXT,
+    -- Metadata
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- envi_act_output
@@ -128,5 +158,8 @@ CREATE TABLE silver.envi_activity_output (
     company_id 				VARCHAR(20),       		    -- Referenced to company_info.
     ea_id 				    VARCHAR(30),                -- Referenced to envi_activity.
     act_output 				INT,
-    year INT
+    year INT,
+    -- Metadata
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
