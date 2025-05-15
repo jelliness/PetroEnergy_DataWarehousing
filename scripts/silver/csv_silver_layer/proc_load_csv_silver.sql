487c2fddf3835aa75fee4c45b5a8cd88230d5cc7
jzrll <112363507+jzrll@users.noreply.github.com>
2025-05-15 15:39:47 +0800

    Upload proc_load_csv_silver

---
+++ scripts/silver/csv_silver_layer/proc_load_csv_silver.sql
@@ -0,0 +1,253 @@
+/*
+===============================================================================
+Stored Procedure: Insert Trimmed Data from Bronze to Silver
+===============================================================================
+Script Purpose:
+    This procedure transfers data from bronze tables to silver tables,
+    trimming string values and handling data transformation.
+    
+    The procedure follows the standard ETL pattern with detailed logging
+    of execution times and error handling.
+===============================================================================
+*/
+
+-- Create or replace the master procedure
+CREATE OR REPLACE PROCEDURE silver.load_csv_silver()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    start_time TIMESTAMP;
+    end_time TIMESTAMP;
+    batch_start_time TIMESTAMP;
+    batch_end_time TIMESTAMP;
+BEGIN
+    batch_start_time := CLOCK_TIMESTAMP();
+    RAISE NOTICE '================================================';
+    RAISE NOTICE 'Loading Silver Layer';
+    RAISE NOTICE '================================================';
+    
+    RAISE NOTICE '------------------------------------------------';
+    RAISE NOTICE 'Loading CSV Tables';
+    RAISE NOTICE '------------------------------------------------';
+    
+    BEGIN
+
+        -- Loading silver.csv_company
+        start_time := CLOCK_TIMESTAMP();
+        RAISE NOTICE '>> Truncating Table: silver.csv_company';
+        TRUNCATE TABLE silver.csv_company;
+        RAISE NOTICE '>> Inserting Data Into: silver.csv_company';
+        INSERT INTO silver.csv_company (
+            company_id,
+		    company_name,
+		    resources,
+			dwh_date_created,
+			dwh_date_updated
+        )
+        SELECT
+            TRIM(company_id),
+            TRIM(company_name),
+            TRIM(resources),
+			dwh_date_created,
+			dwh_date_updated
+        FROM bronze.csv_company;
+        end_time := CLOCK_TIMESTAMP();
+        RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
+        RAISE NOTICE '>> -------------';
+
+		
+		-- Loading silver.csv_emission_factors
+        start_time := CLOCK_TIMESTAMP();
+        RAISE NOTICE '>> Truncating Table: silver.csv_emission_factors';
+        TRUNCATE TABLE silver.csv_emission_factors;
+        RAISE NOTICE '>> Inserting Data Into: silver.csv_emission_factors';
+        INSERT INTO silver.csv_emission_factors (
+			ef_id,
+            generation_source,
+		    kg_co2_per_kwh,
+			dwh_date_created,
+			dwh_date_updated
+        )
+        WITH enriched AS (
+		    SELECT 
+		        LOWER(generation_source::text) AS generation_source,
+		        CAST(kg_co2_per_kwh AS DECIMAL(10,4)) AS kg_co2_per_kwh,
+		        'EF-' || LPAD(ROW_NUMBER() OVER (ORDER BY generation_source)::text, 3, '0') AS ef_id,
+				dwh_date_created,
+				dwh_date_updated
+		    FROM bronze.csv_emission_factors
+		)
+		SELECT ef_id, generation_source,kg_co2_per_kwh, dwh_date_created, dwh_date_updated FROM enriched;
+        end_time := CLOCK_TIMESTAMP();
+        RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
+        RAISE NOTICE '>> -------------';
+
+
+		-- Loading silver.csv_power_plants
+        start_time := CLOCK_TIMESTAMP();
+        RAISE NOTICE '>> Truncating Table: silver.csv_power_plants';
+        TRUNCATE TABLE silver.csv_power_plants;
+        RAISE NOTICE '>> Inserting Data Into: silver.csv_power_plants';
+        INSERT INTO silver.csv_power_plants (
+			power_plant_id,
+		    company_id,
+		    site_name,
+		    site_address,
+		    city_town,
+		    province,
+		    country,
+		    zip,
+			ef_id,
+			dwh_date_created,
+			dwh_date_updated
+        )
+        SELECT 
+		    pp.power_plant_id,
+		    pp.company_id,
+		    pp.site_name,
+		    pp.site_address,
+		    pp.city_town,
+		    pp.province,
+		    pp.country,
+		    pp.zip,
+		    CASE 
+		        WHEN com.resources = 'solar' THEN 'EF-001'
+		        WHEN com.resources = 'wind' THEN 'EF-002'
+		        ELSE null
+		    END AS ef_id,
+			pp.dwh_date_created,
+			pp.dwh_date_updated
+		FROM bronze.csv_power_plants pp
+		LEFT JOIN bronze.csv_company com ON com.company_id = pp.company_id;
+        end_time := CLOCK_TIMESTAMP();
+        RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
+        RAISE NOTICE '>> -------------';
+
+
+	   -- Loading silver.csv_energy_records
+        start_time := CLOCK_TIMESTAMP();
+        RAISE NOTICE '>> Truncating Table: silver.csv_energy_records';
+        TRUNCATE TABLE silver.csv_energy_records;
+        RAISE NOTICE '>> Inserting Data Into: silver.csv_energy_records';
+        INSERT INTO silver.csv_energy_records (
+		    energy_id,
+		    power_plant_id,
+		    date_generated,
+		    energy_generated_kwh,
+		    co2_avoidance_kg,
+		    dwh_date_created,
+		    dwh_date_updated
+		)
+		WITH standardized_data AS(
+			SELECT
+				power_plant_id,
+				to_timestamp(datetime, 'DD/MM/YYYY HH24:MI') AS datetime_column,
+				energy_generated,
+				CASE 
+					WHEN unit_of_measurement = 'mwh' THEN REPLACE(energy_generated, ',', '')::NUMERIC * 1000
+					ELSE REPLACE(energy_generated, ',', '')::NUMERIC
+				END AS energy_generated_converted,
+				unit_of_measurement,
+				'kwh' AS standardized_unit,
+				input_frequency
+			FROM bronze.csv_energy_records
+		),
+		ranked_data AS (
+		    SELECT
+		        * ,
+		        ROW_NUMBER() OVER (
+		        PARTITION BY datetime_column::date, power_plant_id
+		        ORDER BY datetime_column ASC
+		    ) AS rn_asc,
+		    ROW_NUMBER() OVER (
+		        PARTITION BY datetime_column::date,power_plant_id
+		        ORDER BY datetime_column DESC
+		    ) AS rn_desc
+		    FROM standardized_data
+		),
+		aggregated_data AS (
+		    SELECT
+		        power_plant_id,
+		        MAX(CASE WHEN rn_asc = 1 THEN energy_generated_converted END) AS first_energy,
+		        MAX(CASE WHEN rn_desc = 1 THEN energy_generated_converted END) AS last_energy,
+		        SUM(energy_generated_converted) AS total_energy,
+		        MIN(datetime_column) AS earliest_datetime
+		    FROM ranked_data
+			GROUP BY power_plant_id,datetime_column::date
+		),
+		energy_id_generation AS (
+		    SELECT
+		        * ,
+		        'EN-' || TO_CHAR(earliest_datetime, 'YYYYMMDD') || '-' || LPAD(ROW_NUMBER() OVER (ORDER BY earliest_datetime)::TEXT, 3, '0') AS energy_id
+		    FROM aggregated_data
+		),
+		energy_calculation AS(
+			SELECT 
+				eg.energy_id,
+				eg.power_plant_id,
+				eg.earliest_datetime as datetime,
+				ROUND(
+					CASE 
+						WHEN cp.resources='solar' THEN eg.last_energy - eg.first_energy
+						WHEN cp.resources='wind' THEN eg.total_energy
+						ELSE NULL
+					END, 4
+				) AS energy_generated
+			FROM energy_id_generation eg
+			LEFT JOIN bronze.csv_power_plants pp on pp.power_plant_id = eg.power_plant_id 
+			LEFT JOIN bronze.csv_company cp on cp.company_id = pp.company_id
+		),
+		pp_data AS (
+		    SELECT 
+		        pp.power_plant_id,
+		        pp.company_id,
+		        pp.site_name,
+		        pp.site_address,
+		        pp.city_town,
+		        pp.province,
+		        pp.country,
+		        pp.zip,
+		        LOWER(com.resources) AS resource,
+		        CASE 
+		            WHEN LOWER(com.resources) = 'solar' THEN 'EF-001'
+		            WHEN LOWER(com.resources) = 'wind' THEN 'EF-002'
+		            ELSE NULL
+		        END AS constants
+		    FROM bronze.csv_power_plants pp
+		    LEFT JOIN bronze.csv_company com ON com.company_id = pp.company_id
+		),
+		
+		emission_factors AS (
+		    SELECT 
+		        'EF-' || LPAD(ROW_NUMBER() OVER (ORDER BY LOWER(generation_source))::TEXT, 3, '0') AS ef_id,
+		        LOWER(generation_source) AS generation_source,
+		        kg_co2_per_kwh
+		    FROM bronze.csv_emission_factors
+		)
+		SELECT 
+		    ce.energy_id,
+		    ce.power_plant_id,
+		    ce.datetime,
+		    ce.energy_generated,
+		    ROUND(ce.energy_generated * ef.kg_co2_per_kwh::NUMERIC, 4) AS co2_avoidance,
+		    NOW() AS dwh_date_created,
+		    NOW() AS dwh_date_updated
+		FROM energy_calculation ce
+		LEFT JOIN pp_data pp ON pp.power_plant_id = ce.power_plant_id
+		LEFT JOIN emission_factors ef ON ef.ef_id = pp.constants
+		ORDER BY energy_id;
+        end_time := CLOCK_TIMESTAMP();
+        RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
+        RAISE NOTICE '>> -------------';
+        
+        
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE NOTICE '==========================================';
+            RAISE NOTICE 'ERROR OCCURRED DURING LOADING SILVER LAYER';
+            RAISE NOTICE 'Error Message: %', SQLERRM;
+            RAISE NOTICE 'Error Code: %', SQLSTATE;
+            RAISE NOTICE '==========================================';
+    END;
+END;
+$$;
