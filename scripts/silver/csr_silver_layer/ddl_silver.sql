b23c61888eb5380c965d90f09053e45076de8042
Machoo28 <johnmatnepo28@gmail.com>
2025-05-15 12:43:12 +0800

    fixed and updated csr

--- scripts/silver/csr_silver_layer/ddl_silver.sql
+++ scripts/silver/csr_silver_layer/ddl_silver.sql
@@ -7,14 +7,50 @@
 --     Run this script to re-define the DDL structure of 'silver' Tables
 -- ===============================================================================
 
-DROP TABLE IF EXISTS silver.csr_accomplishments;
-
-CREATE TABLE silver.csr_accomplishments (
-    ac_id TEXT NOT NULL,
+DROP TABLE IF EXISTS silver.csr_company;
+CREATE TABLE silver.csr_company (
     company_id TEXT NOT NULL,
-    ac_year TEXT NOT NULL,      
-    csr_program TEXT NOT NULL,
-    csr_report TEXT,            
+    company_name TEXT,
+    resources TEXT,
     date_created TIMESTAMP DEFAULT NOW(),
     date_updated TIMESTAMP DEFAULT NOW()
-);  
+);
+
+DROP TABLE IF EXISTS silver.csr_programs;
+CREATE TABLE silver.csr_programs (
+    program_id TEXT NOT NULL,
+    program_name TEXT,
+    date_created TIMESTAMP DEFAULT NOW(),
+    date_updated TIMESTAMP DEFAULT NOW()
+);
+
+DROP TABLE IF EXISTS silver.csr_projects;
+CREATE TABLE silver.csr_projects (
+    project_id TEXT NOT NULL,
+    program_id TEXT,
+    project_name TEXT,
+    project_metrics TEXT,
+    date_created TIMESTAMP DEFAULT NOW(),
+    date_updated TIMESTAMP DEFAULT NOW()
+);
+
+DROP TABLE IF EXISTS silver.csr_per_company;
+CREATE TABLE silver.csr_per_company (
+    inv_id TEXT NOT NULL,
+    company_id TEXT,
+    program_id TEXT,
+    program_investment TEXT,
+    date_created TIMESTAMP DEFAULT NOW(),
+    date_updated TIMESTAMP DEFAULT NOW()
+);
+
+DROP TABLE IF EXISTS silver.csr_activity;
+CREATE TABLE silver.csr_activity (
+    csr_id TEXT NOT NULL,
+    company_id TEXT NOT NULL,
+    project_id TEXT NOT NULL,
+    ac_year TEXT NOT NULL,
+    csr_report TEXT,
+    date_created TIMESTAMP DEFAULT NOW(),
+    date_updated TIMESTAMP DEFAULT NOW()
+);
