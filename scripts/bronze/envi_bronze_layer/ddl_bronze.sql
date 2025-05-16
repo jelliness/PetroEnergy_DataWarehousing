9be5f0256cdb401244b9b1c4c6b8296f444fdb38
Jayhoodini <91552321+Jayhoodini@users.noreply.github.com>
2025-05-16 14:54:36 +0800

    Modified ddl_bronze.sql
    change max character length per data.

--- scripts/bronze/envi_bronze_layer/ddl_bronze.sql
+++ scripts/bronze/envi_bronze_layer/ddl_bronze.sql
@@ -26,7 +26,7 @@ Script Purpose:
        - Logs diesel consumption per company property, with measurements 
          and dates.
 
-	6. envi_elec_cons
+	10. envi_elec_cons
        - Records electricity consumption data by company or property over time.
 
     7. envi_pow_gen
@@ -54,39 +54,39 @@ Script Purpose:
 -- envi_company_property
 DROP TABLE IF EXISTS bronze.envi_company_property;
 CREATE TABLE bronze.envi_company_property (
-    cp_id      VARCHAR(30),       -- Example: CP-PSC-001
-    company_id VARCHAR(20),       -- Referenced to company_info.
-    cp_name    VARCHAR(100),
-    cp_type    VARCHAR(50)        -- Example values: Equipment, Vehicle
+    cp_id      VARCHAR(20),       -- Example: CP-PSC-001
+    company_id VARCHAR(10),       -- Referenced to company_info.
+    cp_name    VARCHAR(30),
+    cp_type    VARCHAR(15)        -- Example values: Equipment, Vehicle
 );
 
 -- envi_natural_sources
 DROP TABLE IF EXISTS bronze.envi_natural_sources;
 CREATE TABLE bronze.envi_natural_sources (
-    ns_id      VARCHAR(30),       -- Example: NS-PSC-001
-    company_id VARCHAR(20),       -- Referenced to company_info.
-    ns_name    VARCHAR(100)
+    ns_id      VARCHAR(20),       -- Example: NS-PSC-001
+    company_id VARCHAR(10),       -- Referenced to company_info.
+    ns_name    VARCHAR(30)
 );
 
 -- envi_water_withdrawal
 DROP TABLE IF EXISTS bronze.envi_water_withdrawal;
 CREATE TABLE bronze.envi_water_withdrawal (
-    ww_id                 VARCHAR(30),         -- Example: WW-PSC-2022-002
-    company_id            VARCHAR(20),         -- Referenced to company_info.
+    ww_id                 VARCHAR(20),         -- Example: WW-PSC-2022-002
+    company_id            VARCHAR(10),         -- Referenced to company_info.
     year                  SMALLINT,
-    month                 VARCHAR(20),
-    ns_id                 VARCHAR(30),         -- Referenced to natural sources.
-    volume                DOUBLE PRECISION,    -- Allows decimal values (e.g., 123.456)
-    unit_of_measurement   VARCHAR(20)
+    month                 VARCHAR(10),
+    ns_id                 VARCHAR(20),         -- Referenced to natural sources.
+    volume                DOUBLE PRECISION,    -- Allows decimal values (e.g., 123.4510)
+    unit_of_measurement   VARCHAR(15)
 );
 
 -- envi_diesel_consumption
 DROP TABLE IF EXISTS bronze.envi_diesel_consumption;
 CREATE TABLE bronze.envi_diesel_consumption (
-    dc_id                VARCHAR(30),          -- Example: DC-PSC-2024-006
-    company_id           VARCHAR(20),          -- Referenced to company info.
-    cp_id                VARCHAR(30),          -- Referenced to envi_company_property.
-    unit_of_measurement  VARCHAR(20),
+    dc_id                VARCHAR(20),          -- Example: DC-PSC-2024-0010
+    company_id           VARCHAR(10),          -- Referenced to company info.
+    cp_id                VARCHAR(20),          -- Referenced to envi_company_property.
+    unit_of_measurement  VARCHAR(15),
     consumption          DOUBLE PRECISION,     -- Allows decimal values (e.g., 234.789)
     date                 DATE
 );
@@ -94,46 +94,46 @@ CREATE TABLE bronze.envi_diesel_consumption (
 -- envi_electric_consumption
 DROP TABLE IF EXISTS bronze.envi_electric_consumption;
 CREATE TABLE bronze.envi_electric_consumption (
-    ec_id					VARCHAR(30),           -- Example: EC-PSC-2023-001
-    company_id			 	VARCHAR(20),      -- Referenced to company_info.
-    unit_of_measurement	 	VARCHAR(20),
+    ec_id					VARCHAR(20),           -- Example: EC-PSC-2023-001
+    company_id			 	VARCHAR(10),      -- Referenced to company_info.
+    unit_of_measurement	 	VARCHAR(15),
     consumption 		 	DOUBLE PRECISION,     -- Allows decimal values (e.g., 234.789)
-    quarter           	 	VARCHAR(5),
+    quarter           	 	VARCHAR(2),
     year               	 	INT
 );
 
 -- envi_non_hazard_waste
 DROP TABLE IF EXISTS bronze.envi_non_hazard_waste;
 CREATE TABLE bronze.envi_non_hazard_waste (
-    nhw_id 					VARCHAR(30),           -- Example: NHW-PSC-2024-001
-    company_id 				VARCHAR(20),       -- Referenced to company_info.
-    waste_source 			VARCHAR(50),     -- Example: Staff House, Security, Utility
-    metrics 				VARCHAR(20),
-    unit_of_measurement 	VARCHAR(20),
+    nhw_id 					VARCHAR(20),           -- Example: NHW-PSC-2024-001
+    company_id 				VARCHAR(10),       -- Referenced to company_info.
+    waste_source 			VARCHAR(20),     -- Example: Staff House, Security, Utility
+    metrics                 VARCHAR(20),
+    unit_of_measurement 	VARCHAR(15),
     waste 					DOUBLE PRECISION,     -- Allows decimal values (e.g., 234.789)
-    month 					VARCHAR(15),
+    month 					VARCHAR(10),
     year 					INT
 );
 
 -- envi_hazard_waste
 DROP TABLE IF EXISTS bronze.envi_hazard_waste_generated;
 CREATE TABLE bronze.envi_hazard_waste_generated (
-    hwg_id 					   VARCHAR(30),          		-- Example: HW-PSC-2023-001
-    company_id 				VARCHAR(20),      		-- Referenced to company_info.
-    metrics 				   VARCHAR(20),
-    unit_of_measurement 	VARCHAR(20),
-    waste_generated 			DOUBLE PRECISION,     -- Allows decimal values (e.g., 234.789)
-    quarter 				   VARCHAR(2),  		 		-- Example: 'Q1', 'Q2', 'Q3', 'Q4'
-    year 					   INT
+    hwg_id 					VARCHAR(20),          		-- Example: HW-PSC-2023-001
+    company_id 				VARCHAR(10),      		-- Referenced to company_info.
+    metrics                 VARCHAR(20),
+    unit_of_measurement 	VARCHAR(15),
+    waste_generated 		DOUBLE PRECISION,     -- Allows decimal values (e.g., 234.789)
+    quarter 				VARCHAR(2),  		 		-- Example: 'Q1', 'Q2', 'Q3', 'Q4'
+    year 					INT
 );
 
 -- envi_hazard_waste
 DROP TABLE IF EXISTS bronze.envi_hazard_waste_disposed;
 CREATE TABLE bronze.envi_hazard_waste_disposed (
-    hwd_id 					   VARCHAR(30),          		-- Example: HW-PSC-2023-001
-    company_id 				VARCHAR(20),      		-- Referenced to company_info.
-    metrics 				   VARCHAR(20),
-    unit_of_measurement 	VARCHAR(20),
+    hwd_id 					VARCHAR(20),          		-- Example: HW-PSC-2023-001
+    company_id 				VARCHAR(10),      		-- Referenced to company_info.
+    metrics                 VARCHAR(20),
+    unit_of_measurement 	VARCHAR(15),
     waste_disposed 			DOUBLE PRECISION,     -- Allows decimal values (e.g., 234.789)
     year 				   	INT
 );
