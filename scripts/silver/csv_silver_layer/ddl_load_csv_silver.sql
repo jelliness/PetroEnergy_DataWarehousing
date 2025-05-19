1888401edac06f1ca6dfa1a15be3f4ed31705e50
jzrll <jetjetllumbera@gmail.com>
2025-05-19 14:51:58 +0800

    Adjust field sizes in silver csv

--- scripts/silver/csv_silver_layer/ddl_load_csv_silver.sql
+++ scripts/silver/csv_silver_layer/ddl_load_csv_silver.sql
@@ -9,7 +9,7 @@ DDL Script: Create Silver Tables for Power Plant Data Warehouse
 -- ======================================
 DROP TABLE IF EXISTS silver.csv_emission_factors CASCADE;
 CREATE TABLE silver.csv_emission_factors (
-    ef_id VARCHAR(20) PRIMARY KEY,           				-- PK: Unique emission factor ID
+    ef_id VARCHAR(10) PRIMARY KEY,           				-- PK: Unique emission factor ID
     generation_source TEXT,                    			-- Type of generation source
     kg_co2_per_kwh DECIMAL(10,5),               			-- CO2 emission per kWh
     create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -29,7 +29,7 @@ CREATE TABLE silver.csv_power_plants (
     province VARCHAR(30),
     country VARCHAR(30),
     zip VARCHAR(4),
-    ef_id VARCHAR(20) NOT NULL,                        	-- FK: References csv_emission_factors
+    ef_id VARCHAR(10) NOT NULL,                        	-- FK: References csv_emission_factors
     create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
@@ -46,7 +46,7 @@ CREATE TABLE silver.csv_power_plants (
 DROP TABLE IF EXISTS silver.csv_energy_records CASCADE;
 CREATE TABLE silver.csv_energy_records (
     energy_id VARCHAR(20) PRIMARY KEY,          			-- PK: Unique energy record ID
-    power_plant_id VARCHAR(20) NOT NULL,                 	-- FK: References csv_power_plants
+    power_plant_id VARCHAR(10) NOT NULL,                 	-- FK: References csv_power_plants
     date_generated TIMESTAMP NOT NULL,
     energy_generated_kwh NUMERIC(15, 4) NOT NULL DEFAULT 0,
     co2_avoidance_kg NUMERIC(15, 4) NOT NULL DEFAULT 0,
@@ -63,7 +63,7 @@ CREATE TABLE silver.csv_energy_records (
 -- ======================================
 DROP TABLE IF EXISTS silver.csv_fa_factors CASCADE;
 CREATE TABLE silver.csv_fa_factors (
-    ff_id VARCHAR(20) PRIMARY KEY,
+    ff_id VARCHAR(10) PRIMARY KEY,
     ff_name TEXT,
     ff_percentage DECIMAL(5,4),
     create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -75,7 +75,7 @@ CREATE TABLE silver.csv_fa_factors (
 -- ======================================
 DROP TABLE IF EXISTS silver.csv_hec_factors CASCADE;
 CREATE TABLE silver.csv_hec_factors (
-    hec_id VARCHAR(20) PRIMARY KEY,
+    hec_id VARCHAR(10) PRIMARY KEY,
     hec_value DECIMAL(10,4) NOT NULL DEFAULT 0,
     hec_year SMALLINT NOT NULL,
     source_name TEXT,
