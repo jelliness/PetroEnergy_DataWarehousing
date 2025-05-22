31e8d7adc16f15a5e27bbffcf98223fe36265585
jelliness <jellyannekaye0504@gmail.com>
2025-05-22 13:46:10 +0800

    moved company_main to public schema

--- scripts/public/ddl.sql
+++ scripts/public/ddl.sql
@@ -2,15 +2,8 @@
 DROP TABLE IF EXISTS ref.company_main;
 
 CREATE TABLE ref.company_main (
-    company_id VARCHAR(20) PRIMARY KEY,
+    company_id VARCHAR(10) PRIMARY KEY,
     company_name VARCHAR(255) NOT NULL,
     parent_company_id VARCHAR(20) REFERENCES ref.company_main(company_id) ON DELETE SET NULL,
     address TEXT
 );
-
-DROP TABLE IF EXISTS ref.expenditure_type;
-CREATE TABLE ref.expenditure_type (
-    type_id VARCHAR(4) PRIMARY KEY,
-    type_description TEXT
-);
-
--- scripts/ref/ddl.sql
+++ scripts/ref/ddl.sql
@@ -4,16 +4,7 @@ DDL Script: Create ref Tables
 ===============================================================================
 */
 
--- CREATE SCHEMA ref;
-DROP TABLE IF EXISTS ref.company_main CASCADE;
-
-CREATE TABLE ref.company_main (
-    company_id VARCHAR(20) PRIMARY KEY,
-    company_name VARCHAR(255) NOT NULL,
-    parent_company_id VARCHAR(20) REFERENCES ref.company_main(company_id) ON DELETE SET NULL,
-    address TEXT
-);
-
+CREATE SCHEMA ref;
 DROP TABLE IF EXISTS ref.expenditure_type CASCADE;
 CREATE TABLE ref.expenditure_type (
     type_id VARCHAR(4) PRIMARY KEY,
@@ -54,7 +45,7 @@ CREATE TABLE ref.ref_power_plants (
 
     -- Foreign Keys
     CONSTRAINT fk_power_plant_company FOREIGN KEY (company_id)
-        REFERENCES ref.company_main (company_id) ON DELETE CASCADE,
+        REFERENCES public.company_main (company_id) ON DELETE CASCADE,
     CONSTRAINT fk_power_plant_emission FOREIGN KEY (ef_id)
         REFERENCES ref.ref_emission_factors (ef_id) ON DELETE SET NULL
 );
