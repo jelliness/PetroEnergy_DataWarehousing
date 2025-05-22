59faa28d74359dd554ea52d77fc35aaec3f4d615
jelliness <jellyannekaye0504@gmail.com>
2025-05-22 11:33:30 +0800

    moved constants from csv to ref

--- scripts/ref/load_ref.sql
+++ scripts/ref/load_ref.sql
@@ -13,6 +13,44 @@ BEGIN
     SET TIME ZONE 'Asia/Manila';
     RAISE NOTICE 'Session time zone set to %', current_setting('TIMEZONE');
 
+    RAISE NOTICE '>> Resetting company_main table...';
+    
+
+	INSERT INTO ref.company_main (company_id, company_name, parent_company_id, address) VALUES
+	    ('PERC', 'PetroEnergy Resources Corp', NULL, NULL),
+	    ('PGEC', 'PetroGreen Energy Corp', 'PERC', NULL),
+	    ('PSC', 'PetroSolar Corp', 'PGEC', NULL),
+	    ('PWEI', 'PetroWind Energy Inc.', 'PGEC', NULL),
+	    ('MGI', 'Maibarara Geothermal Inc.', 'PGEC', NULL),
+	    ('ESEC', 'EcoSolar Energy Corp', 'PGEC', NULL),
+	    ('RGEC', 'Rizal Green Energy Corp', 'PGEC', NULL),
+	    ('BEP_NL', 'Buhawind Energy Phillippines (Northern Luzon)', 'PGEC', NULL),
+	    ('BEP_NM', 'Buhawind Energy Phillippines (Northern Mindoro)', 'PGEC', NULL),
+	    ('BEP_EP', 'Buhawind Energy Phillippines (East Panay)', 'PGEC', NULL),
+	    ('BGEC', 'Bugallon Green Energy Corp', 'PGEC', NULL),
+	    ('SJGEC', 'San Jose Green Energy Corp', 'PGEC', NULL),
+	    ('DGEC', 'Dagohoy Green Energy Corp', 'PGEC', NULL),
+	    ('BKS', 'BKS Green Energy Corp', 'PGEC', NULL)
+	ON CONFLICT (company_id) DO UPDATE
+	SET company_name = EXCLUDED.company_name,
+	    parent_company_id = EXCLUDED.parent_company_id,
+	    address = EXCLUDED.address;
+
+
+    -- ====================
+    -- Upsert expenditure_type
+    -- ====================
+	RAISE NOTICE '>> Upserting expenditure_type table...';
+	
+	INSERT INTO ref.expenditure_type (type_id, type_description) VALUES
+	    ('CS', 'Cost of Sales'),
+	    ('GA', 'General and Administrative')
+	ON CONFLICT (type_id) DO UPDATE
+	SET
+	    type_description = EXCLUDED.type_description;
+
+
+
     
     -- ====================
     -- Load ref_emission_factors
@@ -217,9 +255,6 @@ EXCEPTION
         RAISE NOTICE '================================';
 END;
 $BODY$;
-
-ALTER PROCEDURE ref.load_ref(TEXT)
-OWNER TO postgres;
 
 
 -- for comparison 
