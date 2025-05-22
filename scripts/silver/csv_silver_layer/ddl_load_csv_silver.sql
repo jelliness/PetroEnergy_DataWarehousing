04bc77500270fa7569a3ed2a75ee981808b2f17d
jelliness <jellyannekaye0504@gmail.com>
2025-05-22 12:36:44 +0800

    adjusted silver layer for csv (energy)

--- scripts/silver/csv_silver_layer/ddl_load_csv_silver.sql
+++ scripts/silver/csv_silver_layer/ddl_load_csv_silver.sql
@@ -5,42 +5,6 @@ DDL Script: Create Silver Tables for Power Plant Data Warehouse
 */
 
 -- ======================================
--- Table: silver.csv_emission_factors
--- ======================================
-DROP TABLE IF EXISTS silver.csv_emission_factors CASCADE;
-CREATE TABLE silver.csv_emission_factors (
-    ef_id VARCHAR(10) PRIMARY KEY,           				-- PK: Unique emission factor ID
-    generation_source TEXT,                    			-- Type of generation source
-    kg_co2_per_kwh DECIMAL(10,5),               			-- CO2 emission per kWh
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- ======================================
--- Table: silver.csv_power_plants
--- ======================================
-DROP TABLE IF EXISTS silver.csv_power_plants CASCADE;
-CREATE TABLE silver.csv_power_plants (
-    power_plant_id VARCHAR(10) PRIMARY KEY,     			-- PK: Unique power plant ID
-    company_id VARCHAR(10),                     			-- FK: References csv_company
-    site_name VARCHAR(50),
-    site_address TEXT,
-    city_town VARCHAR(30),
-    province VARCHAR(30),
-    country VARCHAR(30),
-    zip VARCHAR(4),
-    ef_id VARCHAR(10) NOT NULL,                        	-- FK: References csv_emission_factors
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-    -- Foreign Keys
-    CONSTRAINT fk_power_plant_company FOREIGN KEY (company_id)
-        REFERENCES ref.company_main (company_id) ON DELETE CASCADE,
-    CONSTRAINT fk_power_plant_emission FOREIGN KEY (ef_id)
-        REFERENCES silver.csv_emission_factors (ef_id) ON DELETE SET NULL
-);
-
--- ======================================
 -- Table: silver.csv_energy_records
 -- ======================================
 DROP TABLE IF EXISTS silver.csv_energy_records CASCADE;
@@ -55,35 +19,8 @@ CREATE TABLE silver.csv_energy_records (
 
     -- Foreign Key
     CONSTRAINT fk_energy_power_plant FOREIGN KEY (power_plant_id)
-        REFERENCES silver.csv_power_plants (power_plant_id) ON DELETE CASCADE
+        REFERENCES ref.ref_power_plants (power_plant_id) ON DELETE CASCADE
 );
-
--- ======================================
--- Table: silver.csv_fa_factors
--- ======================================
-DROP TABLE IF EXISTS silver.csv_fa_factors CASCADE;
-CREATE TABLE silver.csv_fa_factors (
-    ff_id VARCHAR(10) PRIMARY KEY,
-    ff_name TEXT,
-    ff_percentage DECIMAL(5,4),
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- ======================================
--- Table: silver.csv_hec_factors
--- ======================================
-DROP TABLE IF EXISTS silver.csv_hec_factors CASCADE;
-CREATE TABLE silver.csv_hec_factors (
-    hec_id VARCHAR(10) PRIMARY KEY,
-    hec_value DECIMAL(10,4) NOT NULL DEFAULT 0,
-    hec_year SMALLINT NOT NULL,
-    source_name TEXT,
-    source_link TEXT,
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
 
 -- ======================================
 -- Table: silver.csv_funds_allocation
@@ -98,3 +35,6 @@ CREATE TABLE silver.csv_funds_allocation (
     create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+ALTER TABLE silver.csv_funds_allocation
+ADD CONSTRAINT unique_fund_allocation_key
+UNIQUE (month_generated, power_plant_id, ff_id);
--- scripts/silver/csv_silver_layer/proc_load_csv_silver.sql
+++ scripts/silver/csv_silver_layer/proc_load_csv_silver.sql
@@ -32,173 +32,12 @@ BEGIN
     
     BEGIN
 
-		
-		-- Upserting into silver.csv_emission_factors
-		start_time := CLOCK_TIMESTAMP();
-		RAISE NOTICE '>> Upserting Data Into: silver.csv_emission_factors';
-		WITH enriched AS (
-		    SELECT 
-		        LOWER(generation_source::text) AS generation_source,
-		        CAST(kg_co2_per_kwh AS DECIMAL(10,4)) AS kg_co2_per_kwh,
-		        'EF-' || LPAD(ROW_NUMBER() OVER (ORDER BY generation_source)::text, 3, '0') AS ef_id,
-		        create_at,
-		        updated_at
-		    FROM bronze.csv_emission_factors
-		)
-		INSERT INTO silver.csv_emission_factors (
-		    ef_id,
-		    generation_source,
-		    kg_co2_per_kwh,
-		    create_at,
-		    updated_at
-		)
-		SELECT ef_id, generation_source, kg_co2_per_kwh, create_at, updated_at FROM enriched
-		ON CONFLICT (ef_id) DO UPDATE
-		SET 
-		    generation_source = EXCLUDED.generation_source,
-		    kg_co2_per_kwh = EXCLUDED.kg_co2_per_kwh,
-		    updated_at = EXCLUDED.updated_at;
-		end_time := CLOCK_TIMESTAMP();
-		RAISE NOTICE '>> Upsert Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
-		RAISE NOTICE '>> -------------';
-
-
-		-- Upserting into silver.csv_power_plants
-		start_time := CLOCK_TIMESTAMP();
-		RAISE NOTICE '>> Upserting Data Into: silver.csv_power_plants';
-		INSERT INTO silver.csv_power_plants (
-		    power_plant_id,
-		    company_id,
-		    site_name,
-		    site_address,
-		    city_town,
-		    province,
-		    country,
-		    zip,
-		    ef_id,
-		    create_at,
-		    updated_at
-		)
-		SELECT 
-		    pp.power_plant_id,
-		    pp.company_id,
-		    pp.site_name,
-		    pp.site_address,
-		    pp.city_town,
-		    pp.province,
-		    pp.country,
-		    pp.zip,
-		    ef_id,
-		    pp.create_at,
-		    pp.updated_at
-		FROM bronze.csv_power_plants pp
-		LEFT JOIN ref.company_main com ON com.company_id = pp.company_id
-		ON CONFLICT (power_plant_id) DO UPDATE
-		SET 
-		    company_id = EXCLUDED.company_id,
-		    site_name = EXCLUDED.site_name,
-		    site_address = EXCLUDED.site_address,
-		    city_town = EXCLUDED.city_town,
-		    province = EXCLUDED.province,
-		    country = EXCLUDED.country,
-		    zip = EXCLUDED.zip,
-		    ef_id = EXCLUDED.ef_id,
-		    updated_at = EXCLUDED.updated_at;
-		end_time := CLOCK_TIMESTAMP();
-		RAISE NOTICE '>> Upsert Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
-		RAISE NOTICE '>> -------------';
-
 
 	    -- Loading silver.csv_energy_records
         -- Upserting into silver.csv_energy_records
 		start_time := CLOCK_TIMESTAMP();
 		RAISE NOTICE '>> Upserting Data Into: silver.csv_energy_records';
-		-- WITH standardized_data AS(
-		-- 	SELECT
-		-- 		power_plant_id,
-		-- 		to_timestamp(datetime, 'DD/MM/YYYY HH24:MI') AS datetime_column,
-		-- 		energy_generated,
-		-- 		CASE 
-		-- 			WHEN unit_of_measurement = 'mwh' THEN REPLACE(energy_generated, ',', '')::NUMERIC * 1000
-		-- 			ELSE REPLACE(energy_generated, ',', '')::NUMERIC
-		-- 		END AS energy_generated_converted,
-		-- 		unit_of_measurement,
-		-- 		'kwh' AS standardized_unit
-				
-		-- 	FROM bronze.csv_energy_records
-		-- ),
-		-- ranked_data AS (
-		--     SELECT
-		--         * ,
-		--         ROW_NUMBER() OVER (
-		--         PARTITION BY datetime_column::date, power_plant_id
-		--         ORDER BY datetime_column ASC
-		--     ) AS rn_asc,
-		--     ROW_NUMBER() OVER (
-		--         PARTITION BY datetime_column::date,power_plant_id
-		--         ORDER BY datetime_column DESC
-		--     ) AS rn_desc
-		--     FROM standardized_data
-		-- ),
-		-- aggregated_data AS (
-		--     SELECT
-		--         power_plant_id,
-		--         MAX(CASE WHEN rn_asc = 1 THEN energy_generated_converted END) AS first_energy,
-		--         MAX(CASE WHEN rn_desc = 1 THEN energy_generated_converted END) AS last_energy,
-		--         SUM(energy_generated_converted) AS total_energy,
-		--         MIN(datetime_column) AS earliest_datetime
-		--     FROM ranked_data
-		-- 	GROUP BY power_plant_id,datetime_column::date
-		-- ),
-		-- energy_id_generation AS (
-		--     SELECT
-		--         * ,
-		--         'EN-' || TO_CHAR(earliest_datetime, 'YYYYMMDD') || '-' || LPAD(ROW_NUMBER() OVER (ORDER BY earliest_datetime)::TEXT, 3, '0') AS energy_id
-		--     FROM aggregated_data
-		-- ),
-		-- energy_calculation AS(
-		-- 	SELECT 
-		-- 		eg.energy_id,
-		-- 		eg.power_plant_id,
-		-- 		eg.earliest_datetime as datetime,
-		-- 		ROUND(
-		-- 			CASE 
-		-- 				WHEN cp.resources='solar' THEN eg.last_energy - eg.first_energy
-		-- 				WHEN cp.resources='wind' THEN eg.total_energy
-		-- 				ELSE NULL
-		-- 			END, 4
-		-- 		) AS energy_generated
-		-- 	FROM energy_id_generation eg
-		-- 	LEFT JOIN bronze.csv_power_plants pp on pp.power_plant_id = eg.power_plant_id 
-		-- 	LEFT JOIN bronze.csv_company cp on cp.company_id = pp.company_id
-		-- ),
-		-- pp_data AS (
-		--     SELECT 
-		--         pp.power_plant_id,
-		--         pp.company_id,
-		--         pp.site_name,
-		--         pp.site_address,
-		--         pp.city_town,
-		--         pp.province,
-		--         pp.country,
-		--         pp.zip,
-		--         LOWER(com.resources) AS resource,
-		--         CASE 
-		--             WHEN LOWER(com.resources) = 'solar' THEN 'EF-001'
-		--             WHEN LOWER(com.resources) = 'wind' THEN 'EF-002'
-		--             ELSE NULL
-		--         END AS constants
-		--     FROM bronze.csv_power_plants pp
-		--     LEFT JOIN bronze.csv_company com ON com.company_id = pp.company_id
-		-- ),
-		
-		-- emission_factors AS (
-		--     SELECT 
-		--         'EF-' || LPAD(ROW_NUMBER() OVER (ORDER BY LOWER(generation_source))::TEXT, 3, '0') AS ef_id,
-		--         LOWER(generation_source) AS generation_source,
-		--         kg_co2_per_kwh
-		--     FROM bronze.csv_emission_factors
-		-- )
+
 		INSERT INTO silver.csv_energy_records (
 		    energy_id,
 		    power_plant_id,
@@ -214,18 +53,19 @@ BEGIN
 				TO_CHAR(TO_TIMESTAMP(REPLACE(datetime, '-', '/'), 'FMMM/FMDD/YYYY HH24:MI')::timestamp, 'YYYYMMDD') || 
 				'-' || 
 				LPAD(
-				ROW_NUMBER() OVER (
-					PARTITION BY TO_CHAR(TO_TIMESTAMP(REPLACE(datetime, '-', '/'), 'FMMM/FMDD/YYYY HH24:MI')::timestamp, 'YYYYMMDD') 
-					ORDER BY TO_TIMESTAMP(REPLACE(datetime, '-', '/'), 'FMMM/FMDD/YYYY HH24:MI')::timestamp
-				)::TEXT, 
-				3, '0')
+					ROW_NUMBER() OVER (
+						PARTITION BY TO_CHAR(TO_TIMESTAMP(REPLACE(datetime, '-', '/'), 'FMMM/FMDD/YYYY HH24:MI')::timestamp, 'YYYYMMDD') 
+						ORDER BY TO_TIMESTAMP(REPLACE(datetime, '-', '/'), 'FMMM/FMDD/YYYY HH24:MI')::timestamp
+					)::TEXT, 
+					3, '0'
+				)
 			AS VARCHAR(20)
 			) AS energy_id,
-			
+
 			er.power_plant_id,
-			
+
 			TO_TIMESTAMP(REPLACE(datetime, '-', '/'), 'FMMM/FMDD/YYYY HH24:MI')::timestamp AS date_generated,
-			
+
 			ROUND(
 				CASE
 					WHEN unit_of_measurement ILIKE 'MWh' THEN COALESCE(energy_generated, 0) * 1000
@@ -233,91 +73,94 @@ BEGIN
 					WHEN unit_of_measurement ILIKE 'kWh' THEN COALESCE(energy_generated, 0)
 					ELSE 0
 				END,
-			  	4
+				4
 			) AS energy_generated,
-			
+
 			ROUND(
 				(
-				CASE
-					WHEN unit_of_measurement ILIKE 'MWh' THEN COALESCE(energy_generated, 0) * 1000
-					WHEN unit_of_measurement ILIKE 'GWh' THEN COALESCE(energy_generated, 0) * 1000000
-					WHEN unit_of_measurement ILIKE 'kWh' THEN COALESCE(energy_generated, 0) 
+					CASE
+						WHEN unit_of_measurement ILIKE 'MWh' THEN COALESCE(energy_generated, 0) * 1000
+						WHEN unit_of_measurement ILIKE 'GWh' THEN COALESCE(energy_generated, 0) * 1000000
+						WHEN unit_of_measurement ILIKE 'kWh' THEN COALESCE(energy_generated, 0)
+						ELSE 0
+					END
+				) * COALESCE(CAST(ef.kg_co2_per_kwh AS NUMERIC), 0) 
+				- 
+				CASE 
+					WHEN ef.generation_source ILIKE 'geothermal' THEN 
+						ROUND(
+							(
+								CASE
+									WHEN unit_of_measurement ILIKE 'MWh' THEN COALESCE(energy_generated, 0) * 1000
+									WHEN unit_of_measurement ILIKE 'GWh' THEN COALESCE(energy_generated, 0) * 1000000
+									WHEN unit_of_measurement ILIKE 'kWh' THEN COALESCE(energy_generated, 0)
+									ELSE 0
+								END
+							) * COALESCE(ef.co2_emitted_kg, 0),
+						4)
 					ELSE 0
-				END
-				) * COALESCE(CAST(ef.kg_co2_per_kwh AS NUMERIC), 0),
+				END,
 				4
 			) AS co2_avoidance,
 			NOW(),
 			NOW()
 
-			FROM bronze.csv_energy_records er
-			LEFT JOIN bronze.csv_power_plants pp ON pp.power_plant_id = er.power_plant_id
-			LEFT JOIN bronze.csv_emission_factors ef ON pp.ef_id = ef.ef_id;
+		FROM bronze.csv_energy_records er
+		LEFT JOIN ref.ref_power_plants pp ON pp.power_plant_id = er.power_plant_id
+		LEFT JOIN ref.ref_emission_factors ef ON pp.ef_id = ef.ef_id
+
+		ON CONFLICT (energy_id) DO UPDATE
+		SET 
+			power_plant_id = EXCLUDED.power_plant_id,
+			date_generated = EXCLUDED.date_generated,
+			energy_generated_kwh = EXCLUDED.energy_generated_kwh,
+			co2_avoidance_kg = EXCLUDED.co2_avoidance_kg,
+			updated_at = NOW();
 		end_time := CLOCK_TIMESTAMP();
 		RAISE NOTICE '>> Upsert Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
 		RAISE NOTICE '>> -------------';
 
-		-- Upserting into silver.csv_fa_factors
+		-- Upserting into silver.csv_funds_allocation
 		start_time := CLOCK_TIMESTAMP();
-		RAISE NOTICE '>> Upserting Data Into: silver.csv_fa_factors';
+		RAISE NOTICE '>> Upserting Data Into: silver.csv_funds_allocation';
 
-		INSERT INTO silver.csv_fa_factors (
-			ff_id,
-			ff_name,
-			ff_percentage,
+		INSERT INTO silver.csv_funds_allocation (
+			month_generated,
+			power_plant_id,
+			ff_id, 
+			power_generated_peso,
+			funds_allocated_peso,
 			create_at,
 			updated_at
 		)
 		SELECT 
-			(ff_id)::VARCHAR(20),
-			ff_name,
-			(ff_percentage)::DECIMAL(5,4),
-			CURRENT_TIMESTAMP,
-			CURRENT_TIMESTAMP
-		FROM bronze.csv_fa_factors
-		ON CONFLICT (ff_id) DO UPDATE 
+			DATE_TRUNC('month', er.date_generated) AS month_generated,
+			pp.power_plant_id,
+			ff.ff_id,
+			ROUND(SUM(er.energy_generated_kwh * 0.01), 2) AS power_generated_peso,
+			ROUND(SUM((er.energy_generated_kwh * 0.01) * ff.ff_percentage), 2) AS funds_allocated_peso,
+			NOW(),
+			NOW()
+		FROM silver.csv_energy_records er
+		LEFT JOIN ref.ref_power_plants pp ON pp.power_plant_id = er.power_plant_id
+		CROSS JOIN ref.ref_fa_factors ff
+		GROUP BY 
+			DATE_TRUNC('month', er.date_generated),
+			pp.power_plant_id,
+			ff.ff_id
+
+		ON CONFLICT (month_generated, power_plant_id, ff_id) DO UPDATE
 		SET 
-			ff_name = EXCLUDED.ff_name,
-			ff_percentage = EXCLUDED.ff_percentage,
-			updated_at = CURRENT_TIMESTAMP;
+			power_generated_peso = EXCLUDED.power_generated_peso,
+			funds_allocated_peso = EXCLUDED.funds_allocated_peso,
+			updated_at = NOW();
 
 		end_time := CLOCK_TIMESTAMP();
 		RAISE NOTICE '>> Upsert Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
 		RAISE NOTICE '>> -------------';
 
-		-- Upserting into silver.csv_hec_factors
-		start_time := CLOCK_TIMESTAMP();
-		RAISE NOTICE '>> Upserting Data Into: silver.csv_hec_factors';
 
-		INSERT INTO silver.csv_hec_factors (
-			hec_id,
-			hec_value,
-			hec_year,
-			source_name,
-			source_link,
-			create_at,
-			updated_at
-		)
-		SELECT 
-			(hec_id)::VARCHAR(20),
-			(hec_value)::DECIMAL(10,4),
-			(hec_year)::INTEGER,
-			source_name,
-			source_link,
-			CURRENT_TIMESTAMP,
-			CURRENT_TIMESTAMP
-		FROM bronze.csv_hec_factors
-		ON CONFLICT (hec_id) DO UPDATE 
-		SET 
-			hec_value = EXCLUDED.hec_value,
-			hec_year = EXCLUDED.hec_year,
-			source_name = EXCLUDED.source_name,
-			source_link = EXCLUDED.source_link,
-			updated_at = CURRENT_TIMESTAMP;
-
-		end_time := CLOCK_TIMESTAMP();
-		RAISE NOTICE '>> Upsert Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
-		RAISE NOTICE '>> -------------';
+		
 
         
     EXCEPTION
@@ -334,29 +177,3 @@ $$;
 
 call silver.load_csv_silver()
 
--- additional table (fund allocation)
-INSERT INTO silver.csv_funds_allocation (
-	month_generated,
-    power_plant_id,
-	ff_id, 
-    power_generated_peso,
-    funds_allocated_peso,
-    create_at,
-    updated_at
-)
-SELECT 
-    DATE_TRUNC('month', er.date_generated) AS month_generated,
-    pp.power_plant_id,
-    ff.ff_id,
-    ROUND(SUM(er.energy_generated_kwh * 0.01), 2) AS power_generated_peso,
-    ROUND(SUM((er.energy_generated_kwh * 0.01) * ff.ff_percentage), 2) AS funds_allocated_peso,
-	NOW(),
-	NOW()
-FROM silver.csv_energy_records er
-LEFT JOIN bronze.csv_power_plants pp ON pp.power_plant_id = er.power_plant_id
-CROSS JOIN bronze.csv_fa_factors ff
-GROUP BY 
-    DATE_TRUNC('month', er.date_generated),
-    pp.power_plant_id,
-    ff.ff_id
-ORDER BY month_generated;
