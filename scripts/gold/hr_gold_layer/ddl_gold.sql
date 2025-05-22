17c4204f746e9177346c2b56a8cd96b6c47a465c
jelliness <jellyannekaye0504@gmail.com>
2025-05-22 21:45:02 +0800

    Update ddl_gold.sql

--- scripts/gold/hr_gold_layer/ddl_gold.sql
+++ scripts/gold/hr_gold_layer/ddl_gold.sql
@@ -19,20 +19,19 @@ CREATE OR REPLACE VIEW gold.dim_employee_descriptions AS
 	    demo.employee_id,
 	    demo.gender,
 	    demo.birthdate,
-	    -- demo.age,
 	    demo.p_np,
 	    demo.position_id,
-	    -- pos.position_name,
+	    pos.position_name,
 	    comp.company_id,
 	    comp.company_name,
 	    ten.start_date,
 	    ten.end_date,
 		ten.tenure_length
 	FROM silver.hr_demographics AS demo
-	
-	-- LEFT JOIN silver.hr_position AS pos ON demo.position_id = pos.position_id
-	LEFT JOIN silver.csr_company AS comp ON demo.company_id = comp.company_id
+	LEFT JOIN silver.hr_position AS pos ON demo.position_id = pos.position_id
+	LEFT JOIN ref.company_main AS comp ON demo.company_id = comp.company_id
 	LEFT JOIN silver.hr_tenure AS ten ON demo.employee_id = ten.employee_id;
+
 /*
 ===============================================================================
 							TRAINING DESCRIPTION
